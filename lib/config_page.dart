ab052285d5606adceb1cb3203ea4e18f8d0a7857
Antetokounpo <antor.232@outlook.com>
2023-09-11 10:11:32 -0400

    Start of config page

---
+++ lib/config_page.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+import 'package:provider/provider.dart';
+import 'package:stravadegarschaud/drink_data.dart';
+
+class ConfigModel extends ChangeNotifier {
+  final configBox = Hive.box(name: 'config');
+
+  Drinker get drinker => Drinker.fromJson(configBox.get('drinker', defaultValue: const Drinker(Sex.male, 0.0).toJson()));
+  
+  void setSex(Sex sex) {
+    configBox['drinker'] = Drinker(sex, drinker.weight);
+    notifyListeners();
+  }
+
+  void setWeight(double weight) {
+    print(weight);
+    configBox['weight'] = Drinker(drinker.sex, weight);
+    notifyListeners();
+  }
+}
+
+class ConfigPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: ChangeNotifierProvider(
+            create: (context) => ConfigModel(),
+            child: Column(
+              children: [
+                SexSelector(),
+                WeightSetter()
+              ],
+            ),
+          )
+        ),
+      ),
+    );
+  }
+}
+
+class SexSelector extends StatelessWidget {
+  final List<Sex> sexList = [Sex.male, Sex.female];
+
+  @override
+  Widget build(BuildContext context) {
+    var config = context.watch<ConfigModel>();
+
+    return DropdownButton<Sex>(
+      value: config.drinker.sex,
+      items: sexList.map((value) => 
+        DropdownMenuItem<Sex>(
+          value: value,
+          child: Text(sexToString(value)),
+        )
+      ).toList(),
+      onChanged: (value) {
+        config.setSex(value!);
+      },
+    );
+  }
+
+  String sexToString(Sex sex) {
+    return sex == Sex.male ? "Homme" : "Femme";
+  }
+}
+
+class WeightSetter extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var config = context.watch<ConfigModel>();
+
+    return TextFormField(
+      decoration: const InputDecoration(
+        labelText: "Poids (kg)"
+      ),
+      onChanged: ((value) {
+        try {
+          config.setWeight(double.parse(value));
+        } catch (e) {} // If invalid value, just don't save it
+      }),
+      keyboardType: TextInputType.number,
+      initialValue: config.drinker.weight.toString(),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:hive/hive.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:stravadegarschaud/config_page.dart';
 
 import 'activity_page.dart';
 
@@ -13,6 +15,21 @@ void main() async {
   runApp(const MyApp());
 }
 
+
+final _router = GoRouter(
+  initialLocation: '/activity',
+  routes: [
+    GoRoute(
+      path: '/activity',
+      builder: (context, state) => const ActivityPage(),
+    ),
+    GoRoute(
+      path: '/config',
+      builder: (context, state) => ConfigPage(),
+    )
+  ]
+);
+
 class MyApp extends StatelessWidget {
   const MyApp({
     super.key,
@@ -20,13 +37,13 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
       title: "Strava de gars chaud",
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
         useMaterial3: true,
       ),
-      home: const ActivityPage(),
+      routerConfig: _router,
     );
   }
 }
