e1b6e618cc1e0c08467daee3e2e706069b4babe4
Antetokounpo <antor.232@outlook.com>
2023-10-19 15:45:00 -0400

    Fix error in test file

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:strava/main.dart';
+import 'package:stravadegarschaud/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
