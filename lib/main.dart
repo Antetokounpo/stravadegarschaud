e94805079024e7ed199c4c625bd0478917240e38
Antetokounpo <antor.232@outlook.com>
2023-10-30 14:15:01 -0400

    Add Firebase to project

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,9 @@ import 'package:hive/hive.dart';
 import 'package:provider/provider.dart';
 import 'package:path_provider/path_provider.dart';
 
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
 import 'pages/home_page.dart';
 import 'models/app_model.dart';
 import 'common/brosse_autosaver.dart';
@@ -12,6 +15,10 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   final dir = await getApplicationDocumentsDirectory();
   Hive.defaultDirectory = dir.path;
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   runApp(const MyApp());
 }
